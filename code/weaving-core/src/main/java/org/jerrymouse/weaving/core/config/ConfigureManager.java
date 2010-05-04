3978b2d938b7080bbd83c6ca1e9892793853174b
kaiyannju <kaiyannju@localhost>
2010-05-04 18:42:03 +0800

    jj

---
+++ code/weaving-core/src/main/java/org/jerrymouse/weaving/core/config/ConfigureManager.java
@@ -0,0 +1,8 @@
+package org.jerrymouse.weaving.core.config;
+
+public class ConfigureManager {
+
+	public Configure getConfigure(String classpath) {
+		return null;
+	}
+}
---
+++ doc/开发环境/ubuntu910.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+#添加tortoisehg和mercurial的源
+sudo add-apt-repository ppa:tortoisehg-ppa/releases
+sudo add-apt-repository ppa:mercurial-ppa/releases
+sudo apt-get update
+#安装tortoisehg和mercurial的源
+sudo apt-get install mercurial python-nautilus tortoisehg
+#集成nautilus
+mkdir -p ~/.nautilus/tortoisehg/src/ && cd ~/.nautilus/tortoisehg/src/
+hg clone https://bitbucket.org/tortoisehg/stable tortoisehg
+mkdir -p ~/.nautilus/python-extensions/
+ln -s ~/.nautilus/tortoisehg/src/tortoisehg/contrib/nautilus-thg.py ~/.nautilus/python-extensions/
+#安装sun-jdk
+sudo apt-get install sun-java6-jdk maven
+#打开Firefox查看Google Code页面如何来下代码
+firefox http://code.google.com/p/friendmap/source/checkout
+
