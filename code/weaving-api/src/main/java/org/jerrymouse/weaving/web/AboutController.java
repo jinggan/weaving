0047e83a5e11fbb5d3cf77d96f213c866be1a77b
kaiyannju <kaiyannju@localhost>
2010-05-27 00:10:40 +0800

    d

--- code/weaving-api/src/main/java/org/jerrymouse/weaving/web/AboutController.java
+++ code/weaving-api/src/main/java/org/jerrymouse/weaving/web/AboutController.java
@@ -1,5 +1,12 @@
 package org.jerrymouse.weaving.web;
 
-public class AboutController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class AboutController {
+	@RequestMapping("/about")
+	public String api() {
+		return "about";
+	}
 }
