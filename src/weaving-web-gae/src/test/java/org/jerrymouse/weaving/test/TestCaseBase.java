f8fa3854060ca45e0aaf8964f21dd474540c466b
kaiyannju <kaiyannju@localhost>
2010-06-01 15:01:13 +0800

    h

--- src/weaving-web-gae/src/test/java/org/jerrymouse/weaving/test/TestCaseBase.java
+++ src/weaving-web-gae/src/test/java/org/jerrymouse/weaving/test/TestCaseBase.java
@@ -10,7 +10,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(locations = { "classpath:spring-eye.xml",
 		"classpath:spring-extracter.xml", "classpath:spring-digger.xml",
 		"classpath:spring-repository.xml", "classpath:spring-api.xml",
-		"classpath:spring-web.xml", "classpath:spring-web-gae.xml" })
+		"classpath:spring-web.xml", "classpath:spring-web-gae.xml",
+		"classpath:spring-getfavicon.xml" })
 public abstract class TestCaseBase {
 	protected Log log = LogFactory.getLog(getClass());
 }
